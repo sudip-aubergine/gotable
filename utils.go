b2d982b3502e803c858d7234f0657b3e4aeb9493
Sudip Raval <rsudip90.dev@gmail.com>
2017-03-06 17:04:46 +0530

    FIX: multilinetext, break with newline, replace tab(\t) with zerowidth whitespace

--- utils.go
+++ utils.go
@@ -15,7 +15,10 @@ import (
 
 // REF: http://stackoverflow.com/questions/37290693/how-to-remove-redundant-spaces-whitespace-from-a-string-in-golang
 func standardizeSpaces(s string) string {
-	return strings.Join(strings.Fields(s), " ")
+	// remove only tab character as of now
+	// don't touch rest of the unicode spaces
+	return strings.Replace(s, "\t", "", -1)
+	// return strings.Join(strings.Fields(s), " ")
 }
 
 // getMultiLineText used to get multi line texts,
@@ -32,28 +35,35 @@ func getMultiLineText(v string, colWidth int) ([]string, int) {
 	}
 
 	// get multi line chunk in form of array
-	sa := strings.Split(standardizeSpaces(v), " ") // break up the string at the spaces
-	j := 0
+
+	// if there is any new line break in string then split it
+	newLineBrokeTexts := strings.Split(v, "\n")
 	maxColWidth := 0
-	for i := 0; i < len(sa); i++ { // spin through all substrings
-		if len(sa[i]) <= colWidth && i+1 < len(sa) { // if the width of this substring is less than the requested width, and we're not at the end of the list
-			s := sa[i]                         // we know we're adding this one
-			for k := i + 1; k < len(sa); k++ { // take as many as possible
-				if len(s)+len(sa[k])+1 <= colWidth { // if it fits...
-					s += " " + sa[k] // ...add it to the list...
-					i = k            // ...and keep loop in sync
-				} else {
-					break // otherwise, add what we have and then go back to the outer loop
+
+	// iterate over list of newLineBrokeTexts
+	for _, textLine := range newLineBrokeTexts {
+		sa := strings.Split(standardizeSpaces(textLine), " ")
+		j := 0
+		for i := 0; i < len(sa); i++ { // spin through all substrings
+			if len(sa[i]) <= colWidth && i+1 < len(sa) { // if the width of this substring is less than the requested width, and we're not at the end of the list
+				s := sa[i]                         // we know we're adding this one
+				for k := i + 1; k < len(sa); k++ { // take as many as possible
+					if len(s)+len(sa[k])+1 <= colWidth { // if it fits...
+						s += " " + sa[k] // ...add it to the list...
+						i = k            // ...and keep loop in sync
+					} else {
+						break // otherwise, add what we have and then go back to the outer loop
+					}
 				}
+				a = append(a, s)
+			} else {
+				a = append(a, sa[i])
 			}
-			a = append(a, s)
-		} else {
-			a = append(a, sa[i])
+			if len(a[j]) > maxColWidth { // if there's not enough room for the current string
+				maxColWidth = len(a[j]) // then adjust the max column width we need
+			}
+			j++
 		}
-		if len(a[j]) > maxColWidth { // if there's not enough room for the current string
-			maxColWidth = len(a[j]) // then adjust the max column width we need
-		}
-		j++
 	}
 	return a, maxColWidth
 }
